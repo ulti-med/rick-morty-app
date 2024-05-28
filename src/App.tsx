9bc96b9b0083603b0aed1b037c0a1f7e12e2eb43
Filipp Polonskiy <filipp.polonskiy@synapx.co.uk>
2024-05-28 17:52:39 +0100

    chore: remove unused hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-
 import "./App.css";
 import { Search } from "./components/search/Search";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Search />
